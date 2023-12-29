b6f1874357b4b8aa84372cc8b1a8dd6aa84fcd20
rifqifadhlurrr <155166720+rifqifadhlurrr@users.noreply.github.com>
2023-12-29 20:48:29 +0700

    Add files via upload

---
+++ kasir-main/src/testkasir3/TestKasir3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testkasir3;
+
+/**
+ *
+ * @author hendra
+ */
+public class TestKasir3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kasir a = new Kasir();
+        a.setVisible(true);
+    }
+    
+}
